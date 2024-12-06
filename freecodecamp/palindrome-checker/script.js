017d14bca060e7fb09e9b3b7ed042560201ebb98
Mykhailo Machavariani <rikomydog@gmail.com>
2024-12-06 19:23:25 +0200

    updated script

--- freecodecamp/palindrome-checker/script.js
+++ freecodecamp/palindrome-checker/script.js
@@ -2,20 +2,20 @@ const input = document.getElementById("text-input");
 const btn = document.getElementById("check-btn");
 const result = document.getElementById("result");
 function submited() {
+    if (input.value === "") {
+        alert("Please input a value");
+        return;
+    }
     const regex = /[\W_]/g;
     let organisedInput = input.value.replace(regex, "").toLowerCase();
     let reversedInput = organisedInput.split("").reverse().join("");
-    if (input.value === "") {
-        alert("Please input a value");
-    }
-    else if (organisedInput === reversedInput) {
+    if (organisedInput === reversedInput) {
         console.log(organisedInput);
         console.log(reversedInput);
         result.innerText = input.value;
         result.innerText = `${input.value} is a palindrome`;
+        return;
     }
-    else {
-        result.innerText = `${input.value} is not a palindrome`;
-    }
+    result.innerText = `${input.value} is not a palindrome`;
 }
 btn.addEventListener("click", submited);
